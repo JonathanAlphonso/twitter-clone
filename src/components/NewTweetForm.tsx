31722a28617914fe9f6dae1f73afef7b84b8344a
Jonathan Alphonso <zoltan3210@Hotmail.com>
2023-06-04 21:31:00 -0400

    looks like twitter now

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,20 @@
+import Button from "./Button";
+import ProfileImage from "./ProfileImage";
+import { useSession } from "next-auth/react";
+
+export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+}
