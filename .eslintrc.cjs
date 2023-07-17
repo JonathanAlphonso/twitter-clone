ba1feb1edbd3fb0a596753a6d89f1df1dd320abc
Jonathan Alphonso <zoltan3210@Hotmail.com>
2023-07-16 22:34:12 -0400

    vercel compile

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,15 +8,16 @@ const config = {
       extends: [
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
+      files: ["*.ts", "*.tsx"],
+      // TODO: Remove if/when this gets fixed
       rules: {
-        "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
         "@typescript-eslint/no-unsafe-call": "off",
         "@typescript-eslint/no-unsafe-return": "off",
-        "@typescript-esline/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/restrict-plus-operands": "off",
       },
-      files: ["*.ts", "*.tsx"],
       parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
       },
